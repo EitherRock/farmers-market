44e83e52e6efdf89c9c75c573b2bf686d411120b
Nathan Lesmann <38919317+EitherRock@users.noreply.github.com>
2025-03-23 09:45:47 -0500

    remove folder

--- farmers-market/vite.config.js
+++ farmers-market/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/farmers-market/', //I think remove this for docker container
   plugins: [
     react(),
     tailwindcss
