92aa0fe035f97b6e55cd062c0ba18c9d802df14d
Nathan Lesmann <38919317+EitherRock@users.noreply.github.com>
2025-03-23 09:26:57 -0500

    agsdgs

--- farmers-market/vite.config.js
+++ farmers-market/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/farmers-market/', //I think remove this for docker container
   plugins: [
     react(),
     tailwindcss
